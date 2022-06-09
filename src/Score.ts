cc122fd574603392c999ceb0335afe4cd9607be0
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-06-09 14:57:06 -0300

    feat: save points in local storage

--- src/Score.ts
+++ src/Score.ts
@@ -13,6 +13,23 @@ export class Score extends GameElement {
 
   public drawImage(): void {}
 
+  private registerPoints() {
+    const points = localStorage.getItem('points');
+
+    if (points) {
+      const oldPoints = JSON.parse(points);
+      const newPoints = {
+        points: this.points,
+        best: oldPoints.best > this.points ? oldPoints.best : this.points
+      };
+      
+      localStorage.setItem('points', JSON.stringify(newPoints));
+    } else {
+      const newPoints = { points: this.points, best: this.points };
+      localStorage.setItem('points', JSON.stringify(newPoints));
+    }
+  }
+
   private setPoints(pipesPositions: PipesPosition[][]) {
     if (Array.isArray(pipesPositions) && pipesPositions.length > 1) {
       const flappyTail = 145;
@@ -30,15 +47,18 @@ export class Score extends GameElement {
       if (secondPipeEnd <= flappyTail && secondPipeStart >= flappyTail - pipeWidth) {
         this.points += 1;
       }
+
+      this.registerPoints();
     }
   }
 
   public action({ pipesPositions }: { pipesPositions: PipesPosition[][] }): void {
+    this.setPoints(pipesPositions);
+
     this.elementInfo.context.font = '25px "Press Start 2P"';
     this.elementInfo.context.textAlign = 'right';
     this.elementInfo.context.fillStyle = '#ffffff';
     this.elementInfo.context.fillText(`${this.points}`, this.destX, this.destY);
 
-    this.setPoints(pipesPositions);
   }
 }
