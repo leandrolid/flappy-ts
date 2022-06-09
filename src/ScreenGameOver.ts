d2c83c6c4d840e534b4bd720316e48979587c17a
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-06-09 16:21:34 -0300

    feat: add current and best points to game over screen

--- src/ScreenGameOver.ts
+++ src/ScreenGameOver.ts
@@ -1,4 +1,4 @@
-import { GameElement, GameElementParams } from './GameElement';
+import { DrawImageParams, GameElement, GameElementParams } from './GameElement';
 
 export class ScreenGameOver extends GameElement {
   constructor(elementInfo: GameElementParams) {
@@ -11,5 +11,20 @@ export class ScreenGameOver extends GameElement {
     this.destY = 100;
     this.destWidth = this.sourceWidth;
     this.destHeight = this.sourceHeight;
+  }
+
+  private drawPoints(points: number, destY: number) {
+    this.elementInfo.context.font = '15px "Press Start 2P"';
+    this.elementInfo.context.textAlign = 'right';
+    this.elementInfo.context.fillStyle = '#533847';
+    this.elementInfo.context.fillText(`${points}`, this.destX + this.destWidth - 22, destY);
+  }
+
+  public drawImage(params?: DrawImageParams): void {
+      super.drawImage(params);
+
+      const points = JSON.parse(localStorage.getItem('points')!);
+      this.drawPoints(points.current, this.destY + 95);
+      this.drawPoints(points.best, this.destY + 135);
   }
 }
